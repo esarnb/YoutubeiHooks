6f5d41e8ca59bde76be435dd09ea781e0bf21d08
esarnb <esarnb@gmail.com>
2021-03-08 02:24:43 +0000

    Added SQLite3, error after record insertion

--- index.js
+++ index.js
@@ -2,9 +2,56 @@ require("colors");
 const { Client } = require("youtubei");
 const youtube = new Client();
 
-const map = new Map();
-map.set("ChilledCow", false);
+const sqlite3 = require('sqlite3').verbose();
+const youtubersOBJ = require("./db/youtubersINIT.json");
+let db = new sqlite3.Database('./db/youtubersDB.db', (err) => {
+    if (err) throw err.message;
+    console.log('Connected to the youtuber database.'.brightGreen);
+});
 
+db.serialize(function() {
+    
+    db.run("CREATE table IF NOT EXISTS youtubersTBL (name TEXT, live BOOL)");
+    let stmt = db.prepare("INSERT into youtubersTBL values (?, ?)");
+    for (x in youtubersOBJ) stmt.run(x, youtubersOBJ[x]);
+})
+
+db.each("SELECT * FROM youtubersTBL", function(err, row) {
+    if (err) throw err;
+    console.log(`Name: ${row.name} Live: ${row.live}`.brightYellow);
+})
+
+db.close((err) => {
+    if (err) throw err.message;
+    console.log('Closed the database connection.'.brightBlue);
+});
+
+// db.serialize(() => {
+//     db.each(`SELECT YoutuberId as id, Name as name FROM youtubers`, (err, row) => {
+//         if (err) {
+//         console.error(err.message);
+//         }
+//         console.log(row.id + "\t" + row.name);
+//     });
+// }, closeDB(db));
+
+
+process.on("SIGINT", async () => {
+    console.log('Closing script & the database connection.'.brightYellow);
+    // closeDB(db, process.exit);
+    process.exit()
+});
+
+function closeDB(db, cb) {
+    db.close((err) => {
+        if (err) throw err.message;
+        console.log('Closed the database connection.'.brightBlue);
+    });
+
+    if (cb) cb();
+}
+
+return;
 setInterval(() => {
         
     map.forEach(async (value, key, map) => {
