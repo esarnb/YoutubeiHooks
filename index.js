cfdb0f7a968cd32d850bc53be2628d1a48f08c97
esarnb <esarnb@gmail.com>
2022-01-04 13:32:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 require("colors");
+
+const { Client:DClient, MessageEmbed, WebhookClient, Intents } = require("discord.js");
+const client = new DClient( { intents: Intents.ALL } );
+client.prefix = ",";
+
+client.on("ready", async () => {
+    let prompt = `[Twitch] ${client.user.tag} is now online!`;
+    console.log(prompt.cyan);
+    client.users.resolve("251091302303662080").send(prompt)
+});
+
+let test = new WebhookClient(process.env.testHookID, process.env.testHookToken);
+
+client.login(process.env.TOKEN).catch((err)=> {
+    if (err) throw err;
+})
+
+
+
 const { Client } = require("youtubei");
 const youtube = new Client();
 
@@ -9,93 +28,72 @@ let db = new sqlite3.Database('./db/youtubersDB.db', (err) => {
     console.log('Connected to the youtuber database.'.brightGreen);
 });
 
-db.serialize(function() {
-    
-    db.run("CREATE table IF NOT EXISTS youtubersTBL (name TEXT, live BOOL)");
-    let stmt = db.prepare("INSERT into youtubersTBL values (?, ?)");
-    for (x in youtubersOBJ) stmt.run(x, youtubersOBJ[x]);
-})
-
-db.each("SELECT * FROM youtubersTBL", function(err, row) {
+db.run("CREATE table IF NOT EXISTS youtubersTBL (NAME TEXT, LIVE BOOLEAN DEFAULT false, UNIQUE(NAME))", function (err) {
     if (err) throw err;
-    console.log(`Name: ${row.name} Live: ${row.live}`.brightYellow);
+    console.log(this);
+    let stmt = db.prepare("INSERT or IGNORE into youtubersTBL values (?, ?)", (err) => { if (err) throw err; });
+    
+    for (x in youtubersOBJ) stmt.run(x, youtubersOBJ[x]);
+
+    setInterval(function() { 
+
+        db.each("SELECT * FROM youtubersTBL", async function (err, row) {
+            if (err) throw err;
+            console.log(`Name: ${row.NAME}: ${row.LIVE}`.brightYellow);
+            // Update the specific youtuber's value for name or live state.
+            let stmt2 = db.prepare("UPDATE youtubersTBL SET LIVE = ? WHERE NAME = ?",  (err) => { if (err) throw err; })
+
+            // Look for the youtuber if they are live.
+            let video = await youtube.findOne(`${row.NAME} live`, {type: "video"})
+            if (!video) return console.log("No Search Result")
+            // If the youtuber found is matching and live...
+            if (video.channel.name === row.NAME && video.isLiveContent) {
+                // if the youtuber was not previously streaming (not (expected false) === not streaming),
+                if (!row.LIVE) {
+                    // Set the youtuber to now streaming (true === streaming)
+                    console.log(row.NAME, "is now Live!".brightCyan);
+                    test.send(row.NAME + " is now Live!");
+                    stmt2.run(true, row.NAME)  // turn from offline to online
+                } else {
+                    console.log(row.NAME, "was already online.");
+                    // handle case if streamer is online and has live content and db shows as online
+
+                }
+                
+            } 
+            // if streamer not found (or) streamer found but no live content (and) streamer in db shows online, emit youtuber went offline.
+            else if ((video.channel.name !== row.NAME || !video.isLiveContent) && row.value ) { 
+                console.log(row.NAME, "is now offline.".brightMagenta);
+                test.send(row.NAME + " is now offline.");
+                stmt2.run(false, row.NAME)  // turn from online to offline
+            } else {
+                // handle case if streamer offline and db shows them as offline: do nothing
+            }
+
+
+
+            //              FIX OLD STATE BY REVERTING LIVE BOOL by code below.
+            // console.log(`${row.NAME} is ${row.LIVE ? "Now Offline" : "Now Online"}`);
+            // let stmt2 = db.prepare("UPDATE youtubersTBL SET LIVE = ? WHERE NAME = ?",  (err) => { if (err) throw err; })
+            // stmt2.run(!row.LIVE, row.NAME)
+
+        }, function(err, count) {
+            if (err) throw err;
+            console.log(`All Done! Count: ${count}`);
+            
+        });
+
+    }, 10000)
+
 })
-
-db.close((err) => {
-    if (err) throw err.message;
-    console.log('Closed the database connection.'.brightBlue);
-});
-
-// db.serialize(() => {
-//     db.each(`SELECT YoutuberId as id, Name as name FROM youtubers`, (err, row) => {
-//         if (err) {
-//         console.error(err.message);
-//         }
-//         console.log(row.id + "\t" + row.name);
-//     });
-// }, closeDB(db));
-
 
 process.on("SIGINT", async () => {
     console.log('Closing script & the database connection.'.brightYellow);
-    // closeDB(db, process.exit);
-    process.exit()
-});
-
-function closeDB(db, cb) {
+    
     db.close((err) => {
         if (err) throw err.message;
-        console.log('Closed the database connection.'.brightBlue);
+        console.log('Closed the database connection.'.brightYellow);
     });
 
-    if (cb) cb();
-}
-
-return;
-setInterval(() => {
-        
-    map.forEach(async (value, key, map) => {
-        let video = await youtube.findOne(`${key} live`, {type: "video"})
-        
-        // If the youtuber found is matching and live,
-        if (video.channel.name === key && video.isLiveContent) {
-            // if the youtuber was not previously streaming (false === not streaming),
-            if (!value) {
-                // Set the youtuber to now streaming (true === streaming)
-                console.log(key, "is now Live!".brightCyan);
-                map.set(key, !value);
-                console.log(video)
-            }
-
-            
-        } 
-        // if streamer goes offline but db shows online, emit youtuber went offline.
-        else if (value) {
-            console.log(key, "is now Offline!".brightMagenta);
-            map.set(key, !value);
-        }
-
-        console.log("INTERVAL EXECUTED".grey);
-    })
-}, 10000);
-
-/*
-
-return;
-(async () => {
-    let video = await youtube.findOne(`${channelName} live`, {type: "video"})
-    // const videos = await youtube.search("Never gonna give you up", {
-    //     type: "video", // video | playlist | channel | all
-    // });
-
-    
-    if (video.channel.name === channelName && video.isLiveContent) {
-        console.log("FOUND".brightGreen);
-        console.log(video)
-    } else {
-        console.log("NOT FOUND".brightRed);
-        console.log(video)
-    }
-})();
-
-*/
+    process.exit()
+});
